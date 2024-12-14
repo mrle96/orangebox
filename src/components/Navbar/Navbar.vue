b8ce172ff9162df0ee043d190b3d967efa837948
mrle96 <nikolamrsic96@gmail.com>
2024-12-14 19:36:01 +0100

    Working version A/V switch

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -7,8 +7,7 @@
     <nav class="flex w-full items-center border-b relative border-b-orange-500 bg-amber-500/15 py-2 pl-5 pr-4 drop-shadow-md">
         
         <div class="w-fit bg-orange-700 p-2 font-black text-white">ORANGEBOX</div>
-       
-            <button @click="toAboutPage">About</button>
+    
         
         <button class="ml-auto block p-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
--- src/stores/MusicPlayList.js
+++ src/stores/MusicPlayList.js
@@ -1,4 +1,4 @@
-import { ref, computed ,watch} from "vue";
+import { ref, computed, watch } from "vue";
 import { defineStore } from "pinia";
 
 export const useMusicPlayList = defineStore("musicPlayList", () => {
@@ -6,20 +6,19 @@ export const useMusicPlayList = defineStore("musicPlayList", () => {
   const currentPlayingIndex = ref(0);
   const arrOfSongs = ref([]);
 
-  const resetPlaying=()=>{
-    currentPlaying.value=0;
-    currentPlayingIndex.value=undefined;
-    arrOfSongs.value=arrOfSongs.value.map((song)=>{
-      song.isPlaying=false;
+  const resetPlaying = () => {
+    currentPlaying.value = 0;
+    currentPlayingIndex.value = undefined;
+    arrOfSongs.value = arrOfSongs.value.map((song) => {
+      song.isPlaying = false;
       return song;
-    })
-  }
+    });
+  };
   const addNewAudioFile = (audioObj) => {
     arrOfSongs.value.push(audioObj);
   };
 
   const setCurrentPlaying = (newCurrentSongIndex) => {
-   
     if (arrOfSongs.value.length > newCurrentSongIndex) {
       currentPlayingIndex.value = newCurrentSongIndex;
       currentPlaying.value = arrOfSongs.value[newCurrentSongIndex];
@@ -38,7 +37,7 @@ export const useMusicPlayList = defineStore("musicPlayList", () => {
       setCurrentPlaying(0); // Resetuje na početak ako je poslednja pesma
     }
   };
-  
+
   const getPrevius = () => {
     if (currentPlayingIndex.value > 0) {
       setCurrentPlaying(currentPlayingIndex.value - 1);
@@ -46,7 +45,7 @@ export const useMusicPlayList = defineStore("musicPlayList", () => {
       setCurrentPlaying(arrOfSongs.value.length - 1); // Prebaci na poslednju pesmu ako je trenutni indeks 0
     }
   };
-  
+
   const getAllSongs = () => {
     return arrOfSongs.value;
   };
